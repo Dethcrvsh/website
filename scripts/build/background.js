a9c43aa7d65c8d4bc73a2bdddbc295d66e6d4002
Dethcrvsh <fabianfabbe@hotmail.se>
2024-03-15 21:13:21 +0100

    progress

--- scripts/build/background.js
+++ scripts/build/background.js
@@ -1,41 +1,108 @@
 var CANVAS = document.getElementById("background");
 var CTX = CANVAS.getContext("2d");
-var NUM_OF_STARS = 50;
-var SYMBOLS = ["@", "#", "$", "%", "&", "0"];
-var MIN_SIZE = 5;
-var MAX_SIZE = 30;
+var STAR_DENSITY = 1 / 3500;
 var stars = [];
+var last_frame_time = 0;
+var Mode;
+(function (Mode) {
+    Mode[Mode["IDLE"] = 0] = "IDLE";
+    Mode[Mode["BLINKING"] = 1] = "BLINKING";
+})(Mode || (Mode = {}));
 var Star = /** @class */ (function () {
     function Star(ctx, x_max, y_max) {
+        this.b = 255;
+        this.mode = Mode.IDLE;
+        this.timer = 0;
         this.ctx = ctx;
-        this.symbol = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)];
-        this.x = Math.floor(Math.random() * x_max);
-        this.y = Math.floor(Math.random() * y_max);
-        this.size = Math.floor(Math.random() * (MAX_SIZE - MIN_SIZE) + MIN_SIZE);
+        this.symbol = Star.SYMBOLS[this.random_int(0, Star.SYMBOLS.length)];
+        this.x = this.random_int(0, x_max);
+        this.y = this.random_int(0, y_max);
+        this.size = this.random_int_exp(Star.MIN_SIZE, Star.MAX_SIZE, 0.1);
+        this.r = this.random_int(150, 255);
+        this.g = this.random_int(150, 255);
     }
-    Star.prototype.draw = function () {
+    Star.prototype.draw = function (delta_time) {
+        switch (this.mode) {
+            case Mode.IDLE: {
+                this.do_idle(delta_time);
+                break;
+            }
+            case Mode.BLINKING: {
+                this.do_blink(delta_time);
+                break;
+            }
+        }
         this.ctx.font = this.size + "px Hack-Bold";
-        console.log(this.size + "px Hack-Bold");
         this.ctx.fillText(this.symbol, this.x, this.y);
     };
+    Star.prototype.do_idle = function (delta_time) {
+        if (this.random_int(0, 100) < 1) {
+            this.mode = Mode.BLINKING;
+        }
+        this.ctx.fillStyle = "rgb(" + this.r + " " + this.g + " " + this.b + ")";
+    };
+    Star.prototype.do_blink = function (delta_time) {
+        if (Star.BLINK_DURATION < this.timer) {
+            this.mode = Mode.IDLE;
+            this.timer = 0;
+            return;
+        }
+        this.timer += delta_time;
+        this.ctx.fillStyle = "rgb(" + (this.r + Star.BLINK_COLOR) + " " + (this.g + Star.BLINK_COLOR) + " " + this.b + ")";
+    };
+    Star.prototype.random_int = function (min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    };
+    // Generate numbers between min and max with increasing unlikelihood
+    Star.prototype.random_int_exp = function (min, max, lambda) {
+        var exponent = 15; // Adjust this parameter to change the bias
+        var range = max - min;
+        var rand = Math.random();
+        var num = min + Math.pow(rand, exponent) * range;
+        return Math.floor(num);
+    };
+    Star.SYMBOLS = ["@", "#", "$", "%", "&", "0"];
+    Star.MIN_SIZE = 8;
+    Star.MAX_SIZE = 25;
+    Star.BLINK_DURATION = 100;
+    // How much the colors should increase on a blink
+    Star.BLINK_COLOR = 75;
     return Star;
 }());
-function draw() {
+function on_mouse_press(event) {
+    var rect = CANVAS.getBoundingClientRect();
+    var x = event.clientX;
+    var y = event.clientY;
+    stars.push(new Star(CTX, CANVAS.width, CANVAS.height));
+    console.log(x + " " + y);
+}
+function draw(timestamp) {
     CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    var delta_time = timestamp - last_frame_time;
     stars.forEach(function (star) {
-        star.draw();
+        star.draw(delta_time);
     });
+    last_frame_time = timestamp;
     requestAnimationFrame(draw);
 }
-if (CTX) {
-    CANVAS.width = 1080;
-    CANVAS.height = 720;
-    CTX.fillStyle = "rgb(255 255 255)";
-    // Initialize the stars
-    for (var i = 0; i < NUM_OF_STARS; i++) {
-        stars.push(new Star(CTX, 1080, 720));
+function init() {
+    var root = document.getElementById("root");
+    if (!root) {
+        return;
     }
-    draw();
+    var rect = root.getBoundingClientRect();
+    CANVAS.width = rect.width;
+    CANVAS.height = document.documentElement.scrollHeight;
+    var num_of_stars = CANVAS.width * CANVAS.height * STAR_DENSITY;
+    // Initialize the stars
+    for (var i = 0; i < num_of_stars; i++) {
+        stars.push(new Star(CTX, CANVAS.width, CANVAS.height));
+    }
+    window.addEventListener("mousedown", function (e) { on_mouse_press(e); });
+}
+if (CTX) {
+    init();
+    requestAnimationFrame(draw);
 }
 else {
     alert("Could not initialize drawing context");
--- scripts/src/background.ts
+++ scripts/src/background.ts
@@ -1,58 +1,145 @@
 const CANVAS = document.getElementById("background") as HTMLCanvasElement;
 const CTX = CANVAS.getContext("2d") as CanvasRenderingContext2D;
 
-const NUM_OF_STARS: number = 50;
-const SYMBOLS: string[] = ["@", "#", "$", "%", "&", "0"]
-const MIN_SIZE: number = 5;
-const MAX_SIZE: number = 30;
+const STAR_DENSITY: number = 1 / 3500;
 
 let stars: Star[] = [];
+let last_frame_time: number = 0;
 
+
+enum Mode {
+    IDLE,
+    BLINKING
+}
 
 class Star {
+    static SYMBOLS: string[] = ["@", "#", "$", "%", "&", "0"]
+    static MIN_SIZE: number = 8;
+    static MAX_SIZE: number = 25;
+    static BLINK_DURATION: number = 100;
+    // How much the colors should increase on a blink
+    static BLINK_COLOR: number = 75;
+
     private ctx: CanvasRenderingContext2D;
     private symbol: string;
     private x: number;
     private y: number;
     private size: number;
+    private r: number;
+    private g: number;
+    private b: number = 255;
+
+    private mode: Mode = Mode.IDLE;
+    private timer: number = 0;
 
     constructor(ctx: CanvasRenderingContext2D, x_max: number, y_max: number) {
         this.ctx = ctx;
-        this.symbol = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)];
-        this.x = Math.floor(Math.random() * x_max);
-        this.y = Math.floor(Math.random() * y_max);
-        this.size = Math.floor(Math.random() * (MAX_SIZE - MIN_SIZE) + MIN_SIZE);
+        this.symbol = Star.SYMBOLS[this.random_int(0, Star.SYMBOLS.length)];
+        this.x = this.random_int(0, x_max);
+        this.y = this.random_int(0, y_max);
+        this.size = this.random_int_exp(Star.MIN_SIZE, Star.MAX_SIZE, 0.1);
+        this.r = this.random_int(150, 255);
+        this.g = this.random_int(150, 255);
     }
 
-    public draw() {
+    public draw(delta_time: number) {
+        switch(this.mode) {
+            case Mode.IDLE: {
+                this.do_idle(delta_time);
+                break;
+            }
+            case Mode.BLINKING: {
+                this.do_blink(delta_time);
+                break;
+            }
+        }
+
         this.ctx.font = this.size + "px Hack-Bold";
-        console.log(this.size + "px Hack-Bold");
         this.ctx.fillText(this.symbol, this.x, this.y);
+    }
+
+    private do_idle(delta_time: number) {
+        if (this.random_int(0, 100) < 1) {
+            this.mode = Mode.BLINKING;
+        }
+        
+        this.ctx.fillStyle = "rgb(" + this.r + " " + this.g + " " + this.b + ")";
+    }
+
+    private do_blink(delta_time: number) {
+        if (Star.BLINK_DURATION < this.timer) {
+            this.mode = Mode.IDLE;
+            this.timer = 0;
+            return;
+        }
+
+        this.timer += delta_time;
+        this.ctx.fillStyle = "rgb(" + (this.r + Star.BLINK_COLOR) + " " + (this.g + Star.BLINK_COLOR) + " " + this.b + ")";
+    }
+
+    private random_int(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    // Generate numbers between min and max with increasing unlikelihood
+    private random_int_exp(min: number, max: number, lambda: number) {
+        const exponent = 15; // Adjust this parameter to change the bias
+        const range = max - min;
+        const rand = Math.random();
+        const num = min + Math.pow(rand, exponent) * range;
+        return Math.floor(num);
     }
 }
 
+function on_mouse_press(event) {
+    const rect = CANVAS.getBoundingClientRect();
+    const x = event.clientX;
+    const y = event.clientY;
+    stars.push(new Star(CTX, CANVAS.width, CANVAS.height));
+    console.log(x + " " + y);
+}
 
-function draw() {
+
+function draw(timestamp: number) {
     CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
+
+    const delta_time: number = timestamp - last_frame_time;
+
     stars.forEach(star => {
-        star.draw();
+        star.draw(delta_time);
     });
+
+    last_frame_time = timestamp;
 
     requestAnimationFrame(draw);
 }
 
 
-if (CTX) {
-    CANVAS.width = 1080;
-    CANVAS.height = 720;
-    CTX.fillStyle = "rgb(255 255 255)";
+function init() {
+    const root = document.getElementById("root");
 
-    // Initialize the stars
-    for (let i = 0; i < NUM_OF_STARS; i++) {
-        stars.push(new Star(CTX, 1080, 720));
+    if (!root) {
+        return;
     }
 
-    draw();
+    const rect: DOMRect = root.getBoundingClientRect();
+
+    CANVAS.width = rect.width;
+    CANVAS.height = document.documentElement.scrollHeight;
+    const num_of_stars: number = CANVAS.width * CANVAS.height * STAR_DENSITY;
+
+    // Initialize the stars
+    for (let i = 0; i < num_of_stars; i++) {
+        stars.push(new Star(CTX, CANVAS.width, CANVAS.height));
+    }
+
+    window.addEventListener("mousedown", function(e) { on_mouse_press(e) });
+}
+
+
+if (CTX) {
+    init();
+    requestAnimationFrame(draw);
 } else {
     alert("Could not initialize drawing context");
 }
